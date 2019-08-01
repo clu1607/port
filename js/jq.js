2a466dae95791b895e756c3a9d3f2c3041e94e43
Catherine <catherinelu12@yahoo.com>
2019-08-01 01:15:25 -0400

    added js files

---
+++ js/jq.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+        $('nav li').hover(
+        	function(){
+        		$('ul',this).stop().slideDown(200);
+        	},
+        	function(){
+        		$('ul',this).stop().slideUp(200);
+        	});
+    });
---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop()>100)
+     {
+        $('.motion').fadeIn(1000);
+     }
+});
